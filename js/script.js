c3a8ade24a23b7141561046d515ee68436bd69e5
Maksym <maksymilian.makuch7@gmail.com>
2018-02-01 06:39:01 +0100

    chyba ok

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script>
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "0px";
+    }
+}
+</script>
